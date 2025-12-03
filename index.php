40b3a6efa359e8cc2da2fe51ac285e481f87a82b
iqbalhadiwibowo <muhammad1700018101@webmail.uad.ac.id>
2025-12-03 14:13:40 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Coba</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia iure laborum incidunt, velit quis iusto. Consequatur, praesentium ipsa. Consectetur odio vero nam minima perferendis ipsum fugiat. Facere libero deleniti nostrum.</p>
 </body>
 </html>
